05542ca2b1041085fdce8c9c249b234f591eb76f
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-11-07 09:59:29 +0100

    Add rules in greeting

--- lib/display.rb
+++ lib/display.rb
@@ -3,14 +3,18 @@ module Display
   def greeting
     <<~GREETING
       Let's play a game of Mastermind.
-      You'll have to guess the 6 numbers [1-6]
-      chosen by the computer in 12 guesses or less. 
+      You'll have to guess the 4 numbers [1-6]
+      chosen by the computer in 12 guesses or less.
+      [+] means right number at the right place.
+      [-] means right number at a wrong place.
+      [ ] means wrong number.
+      The feedback is not positional.
     GREETING
   end
 
   def error
     <<~ERROR
-      Please enter 6 numbers between 1 and 6
+      Please enter 4 numbers between 1 and 6
       without spaces or other characters.
     ERROR
   end
