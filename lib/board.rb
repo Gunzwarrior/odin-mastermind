1501742576b442bb22950d5494c666f7f58258cd
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-11-08 09:55:52 +0100

    Add view board function

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,22 @@
 
 class Board
-  attr_accessor :progress, :feedback
+  attr_accessor :progress, :feedback, :line, :full_board
 
   def initialize
+    @line = []
+    @full_board = [["1234", "++--"], ["165643", "--++"]]
     @progress = []
     4.times { progress << []}
     @feedback = []
     4.times { feedback << []}
-    p @progress
-    p @feedback
-    full_board
+    p progress
+    p feedback
+    view_board
   end
 
-  def full_board
-
+  def view_board
+    full_board.each_index { |index| 
+    puts "#{index + 1}. #{full_board[index][0]} | #{full_board[index][1]}" }
   end
 
 end
